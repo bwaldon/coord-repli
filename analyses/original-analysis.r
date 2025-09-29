6329ff22a01dbd0b78627ebcbef71920f738445d
Brandon Waldon <32781952+bwaldon@users.noreply.github.com>
2025-09-29 13:49:34 -0400

    import prolific info

--- analyses/original-analysis.r
+++ analyses/original-analysis.r
@@ -6,7 +6,27 @@ library(emmeans)
 library(interactions)
 library(lme4)
 
-cgdata <- read_csv("../data/demo/playerRound.csv")
+prolificInfo_coodination <- read_csv("../data/pipeline/prolific_export_duo.csv") %>%
+  mutate(condition = "coordination")
+prolificInfo_control <- read_csv("../data/pipeline/prolific_export_solo.csv") %>%
+  mutate(condition = "control")
+
+prolificInfo <- rbind(prolificInfo_coodination, prolificInfo_control) %>%
+  filter(`Completion code` %in% c("DMM9REA8", "CMM9REA9", "CRGEGQIS"))
+rm(prolificInfo_coodination, prolificInfo_control)
+
+workers_to_analyze <- prolificInfo$`Participant id`
+
+## Median completion times 
+
+coordination_med_completion_time = median((prolificInfo %>% filter(condition == "coordination"))$`Time taken`) / 60
+control_med_completion_time = median((prolificInfo %>% filter(condition == "control"))$`Time taken`) / 60
+
+players_to_analyze <- (read_csv("../data/pipeline/player.csv") %>%
+  filter(participantIdentifier %in% workers_to_analyze))$id
+  
+cgdata <- read_csv("../data/pipeline/playerRound.csv") %>%
+  filter(playerID %in% players_to_analyze)
 
 cgdata <- cgdata %>%
   mutate(resp = case_when(resp == "Yes" ~ 1,
@@ -35,20 +55,23 @@ Fig3B = ggplot(Fig3Bdata, aes(x = reorder(case_type, desc(case_type)), y = Trans
                  position = position_dodge(width = .8))+
   geom_point(size = 3, stroke = 1,
              position = position_dodge(width = .8), fill = 'white')+
-  theme_classic()+
-  ylab('')+xlab(NULL)+
+  theme_classic()+xlab(NULL)+
   theme(text = element_text(size = 11),
-        legend.position = c(.5, .85),
+        legend.position = c(.25, .85),
         legend.title=element_blank(),
         legend.background = element_rect(fill = "#F5F5F5", 
                                          color = NA),  
         axis.text.x = element_text(size = 7),
-        axis.text.y = element_blank(),
+        # axis.text.y = element_blank(),
         legend.text = element_text(size = 7),
-        axis.ticks = element_blank(),
+        # axis.ticks = element_blank(),
         plot.margin = margin(18, 1, 1, 1, 'pt'))+
   scale_shape_manual(values = c(24, 21), guide = 'none')+
   xlab(NULL)+scale_y_continuous(breaks = c(0, 0.5, 1), limits = c(0,1))+
-  scale_color_manual(values = c('#1B9E77', '#7570B3'))+
-  scale_fill_manual(values = c('#1B9E77', '#7570B3'))
+  scale_color_manual(values = c('#7570B3', '#1B9E77'))+
+  scale_fill_manual(values = c('#7570B3', '#1B9E77'))+
+  ylab("Transgression judgment")
+
+Fig3B
+
 
